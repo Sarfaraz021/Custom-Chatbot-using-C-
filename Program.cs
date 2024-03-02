12d8196d6bec61c10287face888b9d6d99539be9
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-03-02 16:55:18 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿
+// Importing namespaces
+using Microsoft.SemanticKernel;
+
+namespace Semantic_Kernel_Local_LLM
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("\n======== LM Studio Phi 2 example ========\n");
+
+            var userInput = Console.ReadLine();
+
+            HttpClient client = new HttpClient(handler: new LocalHostServer("http://localhost:1234/v1/chat/completions"));
+
+            Kernel kernel = Kernel.CreateBuilder()
+                .AddAzureOpenAIChatCompletion(
+                    deploymentName: "Phi2",
+                    apiKey: "is not Required",
+                    endpoint: "http://localhost:1234/v1/chat/completions",
+                    httpClient: client)
+                .Build();
+
+
+            // Plugins > Prompts > AvailableIngredients   
+            var plugInDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins\\RecipePrompts");
+            kernel.ImportPluginFromPromptDirectory(plugInDirectory, "RecipePrompts");
+            var AvailableIngredientsFunction = kernel.Plugins.GetFunction(pluginName: "RecipePrompts", functionName: "AvailableIngredients");
+
+            var kernelArguments = new KernelArguments()
+            {
+                {
+                    "input" , userInput
+                }
+            };
+
+            var response = kernel.InvokeStreamingAsync(AvailableIngredientsFunction, kernelArguments);
+            await foreach (var answer in response)
+            {
+                Console.Write(answer);
+            }
+
+
+        }
+    }
+
+    public class LocalHostServer(string url) : HttpClientHandler
+    {
+        private readonly Uri uri = new Uri(url);
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.RequestUri = uri;
+            return base.SendAsync(request, cancellationToken);
+        }
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Custom-Chatbot-using-C-
+# Semantic Kernel in DotNet
+
+## Requirements
+You need to install the following to run this project.
+- Visual Studio 2022 (Community Version)
+- .NET 8 SDK
+
+## LM Studio
+To connect your local LLM to Semantic Kernel, download LM studio. It creates a server through which the application can send request to LLM.
